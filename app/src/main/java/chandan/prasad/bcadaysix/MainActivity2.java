938d0bb0faf7f050a9fbca6553265006a51b3fbb
ChandanSatyendraPrasad <chandansatyendraprasad@gmail.com>
2022-02-01 16:11:19 +0530

    Check BOX

--- app/src/main/java/chandan/prasad/bcadaysix/MainActivity2.java
+++ app/src/main/java/chandan/prasad/bcadaysix/MainActivity2.java
@@ -4,16 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 ImageButton button_image;
+CheckBox checkBox,checkBox1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         button_image=findViewById(R.id.imageButton);
+        checkBox=findViewById(R.id.checkBox);
+        checkBox1=findViewById(R.id.checkBox2);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                {
+                    Toast.makeText(MainActivity2.this, checkBox.getText().toString().trim() + "is Checked", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity2.this, checkBox.getText().toString().trim() + "is Not Checked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         button_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
