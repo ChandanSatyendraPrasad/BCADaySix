4064adbc9d11f277a122173429c2d77a2f27c49a
ChandanSatyendraPrasad <chandansatyendraprasad@gmail.com>
2022-02-04 12:50:05 +0530

    Radio Button Completed

--- app/src/main/java/chandan/prasad/bcadaysix/MainActivity2.java
+++ app/src/main/java/chandan/prasad/bcadaysix/MainActivity2.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 ImageButton button_image;
 CheckBox checkBox,checkBox1;
+RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,27 @@ CheckBox checkBox,checkBox1;
         button_image=findViewById(R.id.imageButton);
         checkBox=findViewById(R.id.checkBox);
         checkBox1=findViewById(R.id.checkBox2);
+        radioGroup=findViewById(R.id.rg);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch(i)
+                {
+                    case R.id.radioButtonMale:
+                        Toast.makeText(MainActivity2.this, "MALE", Toast.LENGTH_SHORT).show();
+                        break;
+
+
+                    case R.id.radioButtonFemale:
+                        Toast.makeText(MainActivity2.this, "FEMALE", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+            }
+        });
+
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
